347dad8137e16ede7a35756cae423c7d2178db28
LeonardoDelBene <leonardo.delbene@stud.unifi.it>
2026-04-23 11:37:19 +0200

    new edit

--- main.py
+++ main.py
@@ -347,12 +347,12 @@ def get_config():
         "seed":                 2043,
         "edit_background":      True,
         "load_existing":        True,
-        "checkpoint_path":      os.path.join("checkpoints", "unet_best_zcc7yv8u.pth"),
+        "checkpoint_path":      os.path.join("checkpoints", "unet_best_qyon0vk7.pth"),
         "attack_model":         "runwayml/stable-diffusion-inpainting",
         "base_output_dir":      "output",
         "dataset_path":         "./data/DiffVaxDataset_local",
         "dataset_split":        "validation",
-        "sample_idx":           5,
+        "sample_idx":           10,
         "run_full_dataset":     False,
     }
 
