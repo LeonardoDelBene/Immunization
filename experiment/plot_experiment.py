ad3dc3036d0cc9a18658efaf05ef9ef10806f930
LeonardoDelBene <leonardo.delbene@stud.unifi.it>
2026-06-16 13:24:52 +0200

    add d3

--- experiment/plot_experiment.py
+++ experiment/plot_experiment.py
@@ -5,21 +5,21 @@ from itertools import cycle
 
 # ── CONFIG ────────────────────────────────────────────────────────────────────
 CSV_PATHS = [
-    "./unet_best_uz0247gg/sweep_eps.csv",
-    "./unet_best_d6msfhlm/sweep_eps.csv",
-    "./unet_best_nv5dqvvb/sweep_eps.csv",
-    "./targeted/sweep_eps.csv",
+    "./unet_best_uz0247gg/sweep_lambda_noise.csv",
+    #"./unet_best_d6msfhlm/sweep_eps.csv",
+    "./unet_best_nv5dqvvb/sweep_lambda_noise.csv",
+    "./unet_best_2k9g9dob/sweep_lambda_noise.csv",
+    #"./targeted/sweep_eps.csv",
 ]
 
 # Optional: set to None to use filenames as labels
 LABELS = [
-    "MSE",
-    "MSE_16",
-    "MSE_FT",
-    "targeted",
+    "Alto noise",
+    "Medio noise",
+    "Basso noise",
 ]
 
-OUTPUT = "eps.png"
+OUTPUT = "d3_lambda_noise.png"
 # ─────────────────────────────────────────────────────────────────────────────
 
 METRIC_GROUPS = [
